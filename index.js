c76a5d0993034fb20305e81b8cd264a6fb04e857
souravq <souravbera515@gmail.com>
2023-10-05 14:22:34 +0530

    single chef end point added

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.get("/chef", (req,res)=>{
     res.send(chefData);
 })
 
-
+app.get("/chef/:id", (req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const ChefData = chefData.filter((data)=>data.id==id);
+    console.log(ChefData);
+    res.send(ChefData);
+})
 
 app.listen(port,()=>{
     console.log(`App is listening at ${port} `);
